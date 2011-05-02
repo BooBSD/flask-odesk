67dbb04bcbb91283f247455a1a5cf77719005a37
Artem Gnilov <boobsd@gmail.com>
2011-05-02 22:56:41 +0300

    Initial import

---
+++ setup.py
@@ -0,0 +1,38 @@
+"""
+Flask-oDesk
+-----------
+
+Adds oDesk API support to Flask.
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-oDesk',
+    version='0.1',
+    url='https://github.com/BooBSD/flask-odesk',
+    license='BSD',
+    author='Artem Gnilov',
+    author_email='boobsd@gmail.com',
+    description='Adds oDesk API support to Flask',
+    long_description=__doc__,
+    packages=['flaskext'],
+    namespace_packages=['flaskext'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'oauth2',
+        'odesk'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
